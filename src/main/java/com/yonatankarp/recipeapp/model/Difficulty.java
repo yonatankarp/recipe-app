1d85f3d9f3f59f9cacfce1d144ea435029e71893
Yonatan Karp-Rudin <yonvata@gmailc.om>
2022-03-11 16:48:22 +0100

    Set Difficulties based on the enum

--- src/main/java/com/yonatankarp/recipeapp/model/Difficulty.java
+++ src/main/java/com/yonatankarp/recipeapp/model/Difficulty.java
@@ -1,5 +1,18 @@
 package com.yonatankarp.recipeapp.model;
 
 public enum Difficulty {
-    EASY, MODERATE, HARD
+    EASY("Easy"),
+    MODERATE("Moderate"),
+    KIND_OF_HARD("Kind of Hard"),
+    HARD("Hard");
+
+    private final String value;
+
+    Difficulty(final String value) {
+        this.value = value;
+    }
+
+    public String value() {
+        return value;
+    }
 }
